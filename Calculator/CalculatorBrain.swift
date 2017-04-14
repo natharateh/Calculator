b9d7834797f358cae1fcd37f3776c7e7eeb37707
nambatee <nambatee@users.noreply.github.com>
2017-04-13 22:13:51 -0400

    add a model

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+struct CalculatorBrain {
+    
+    private var accumulator: Double?
+    
+    mutating func performOperation(_ symbol: String) {
+        switch symbol {
+        case "π":
+            accumulator = Double.pi
+        case "√":
+            if let operand = accumulator {
+                accumulator = sqrt(operand)
+            }
+        default:
+            break
+        }
+    }
+    
+    mutating func setOperand(_ operand: Double) {
+        accumulator = operand
+    }
+    
+    var result: Double? {
+        get {
+            return accumulator
+        }
+    }
+}
